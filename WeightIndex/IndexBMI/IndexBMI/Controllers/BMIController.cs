8aa461eac933e8057a63f384f85862ade77743ab
akadaica4 <82006138+akadaica4@users.noreply.github.com>
2021-09-22 16:58:33 +0700

    Long dao

---
+++ WeightIndex/IndexBMI/IndexBMI/Controllers/BMIController.cs
@@ -0,0 +1,50 @@
+﻿using IndexBMI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IndexBMI.Controllers
+{
+    public class BMIController : Controller
+    {
+        
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new BMIIndex());
+        }
+        [HttpPost]
+        public IActionResult Index(BMIIndex bmiindex)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = bmiindex.Weight / (bmiindex.Height * 2);
+                if(result < 18.5)
+                {
+                    bmiindex.Result = "Thiếu cân";
+                }else if(result < 24.9)
+                {
+                    bmiindex.Result = "Bình thường";
+                }else if(result < 29.9)
+                {
+                    bmiindex.Result = "Thừa cân";
+                }
+                else if (result < 34.9)
+                {
+                    bmiindex.Result = "Béo phì cấp độ 1";
+                }
+                else if (result < 39.9)
+                {
+                    bmiindex.Result = "Béo phì cấp độ 2";
+                }
+                else if (result > 40)
+                {
+                    bmiindex.Result = "Béo phì cấp độ 3";
+                }
+            }
+            return View(bmiindex);
+        }
+    }
+}
---
+++ WeightIndex/IndexBMI/IndexBMI/Models/BMIIndex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IndexBMI.Models
+{
+    public class BMIIndex
+    {
+        [Required(ErrorMessage = "Please enter your height")]
+        [Display(Name = "Height (cm)")]
+        public double Height { get; set; }
+        [Required(ErrorMessage = "Please enter your weight")]
+        [Display(Name = "Weight (kg)")]
+        public double Weight { get; set; }
+        public string Result { get; set; }
+    }
+}
